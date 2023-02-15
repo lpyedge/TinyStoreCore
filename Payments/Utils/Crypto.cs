4d81259315f9561d7f29668f23a7d38307d40fea
lpyedge <18747080@qq.com>
2023-02-15 18:39:36 +0800

    .net core环境下对pem密钥的加载判断出错修复

--- Payments/Utils/Crypto.cs
+++ Payments/Utils/Crypto.cs
@@ -829,12 +829,36 @@ namespace Payments.Utils
         //[SupportedOSPlatform(("windows"))]
         public static RSACryptoServiceProvider FromPEM(string pem)
         {
-            var rsaParams = new CspParameters();
-            rsaParams.Flags = CspProviderFlags.UseMachineKeyStore;
-            var rsa = new RSACryptoServiceProvider(rsaParams);
+            if (pem.Contains("PUBLIC KEY"))
+            { 
+                return  readPublicPem(pem);
+            }
+            else if (pem.Contains("PRIVATE KEY"))
+            {
+                return  readPrivatePem(pem);
+            }
+            else
+            {
+                try
+                {
+                    return readPrivatePem(pem);
+                }
+                catch
+                {
+                }
+                try
+                {
+                    return  readPublicPem(pem);
+                }
+                catch 
+                {
+                }
+                throw new Exception("pem需要BEGIN END标头");
+            }
+        }
 
-            var param = new RSAParameters();
-
+        private static RSACryptoServiceProvider readPrivatePem(string pem)
+        {
             var base64 = Extensions._PEMCode.Replace(pem, "");
             var data = Convert.FromBase64String(base64);
             if (data == null)
@@ -842,7 +866,6 @@ namespace Payments.Utils
                 throw new Exception("PEM内容无效");
             }
             var idx = 0;
-
             //读取长度
             Func<byte, int> readLen = (first) =>
             {
@@ -879,6 +902,7 @@ namespace Payments.Utils
                 idx += len;
                 return val;
             };
+            
             //比较data从idx位置开始是否是byts内容
             Func<byte[], bool> eq = (byts) =>
             {
@@ -895,82 +919,139 @@ namespace Payments.Utils
                 }
                 return true;
             };
-
-
-
-
-            if (pem.Contains("PUBLIC KEY"))
+            
+            /****使用私钥****/
+            //读取数据总长度
+            readLen(0x30);
+            
+            //读取版本号
+            if (!eq(Extensions._Ver))
             {
-                /****使用公钥****/
-                //读取数据总长度
-                readLen(0x30);
-                if (!eq(Extensions._SeqOID))
-                {
-                    throw new Exception("PEM未知格式");
-                }
-                //读取1长度
-                readLen(0x03);
-                idx++;//跳过0x00
-                      //读取2长度
-                readLen(0x30);
-
-                //Modulus
-                param.Modulus = readBlock();
-
-                //Exponent
-                param.Exponent = readBlock();
+                throw new Exception("PEM未知版本");
             }
-            else if (pem.Contains("PRIVATE KEY"))
+            
+            //检测PKCS8
+            var idx2 = idx;
+            if (eq(Extensions._SeqOID))
             {
-                /****使用私钥****/
-                //读取数据总长度
+                //读取1长度
+                readLen(0x04);
+                //读取2长度
                 readLen(0x30);
-
+            
                 //读取版本号
                 if (!eq(Extensions._Ver))
                 {
-                    throw new Exception("PEM未知版本");
+                    throw new Exception("PEM版本无效");
                 }
-
-                //检测PKCS8
-                var idx2 = idx;
-                if (eq(Extensions._SeqOID))
-                {
-                    //读取1长度
-                    readLen(0x04);
-                    //读取2长度
-                    readLen(0x30);
-
-                    //读取版本号
-                    if (!eq(Extensions._Ver))
-                    {
-                        throw new Exception("PEM版本无效");
-                    }
-                }
-                else
-                {
-                    idx = idx2;
-                }
-
-                //读取数据
-                param.Modulus = readBlock();
-                param.Exponent = readBlock();
-                param.D = readBlock();
-                param.P = readBlock();
-                param.Q = readBlock();
-                param.DP = readBlock();
-                param.DQ = readBlock();
-                param.InverseQ = readBlock();
             }
             else
             {
-                throw new Exception("pem需要BEGIN END标头");
+                idx = idx2;
             }
+            var param = new RSAParameters();
+            //读取数据
+            param.Modulus = readBlock();
+            param.Exponent = readBlock();
+            param.D = readBlock();
+            param.P = readBlock();
+            param.Q = readBlock();
+            param.DP = readBlock();
+            param.DQ = readBlock();
+            param.InverseQ = readBlock();
 
+            var rsa = new RSACryptoServiceProvider();
             rsa.ImportParameters(param);
             return rsa;
         }
+        private static RSACryptoServiceProvider readPublicPem(string pem)
+        {
+            var base64 = Extensions._PEMCode.Replace(pem, "");
+            var data = Convert.FromBase64String(base64);
+            if (data == null)
+            {
+                throw new Exception("PEM内容无效");
+            }
+            var idx = 0;
+            //读取长度
+            Func<byte, int> readLen = (first) =>
+            {
+                if (data[idx] == first)
+                {
+                    idx++;
+                    if (data[idx] == 0x81)
+                    {
+                        idx++;
+                        return data[idx++];
+                    }
+                    else if (data[idx] == 0x82)
+                    {
+                        idx++;
+                        return (((int)data[idx++]) << 8) + data[idx++];
+                    }
+                    else if (data[idx] < 0x80)
+                    {
+                        return data[idx++];
+                    }
+                }
+                throw new Exception("PEM未能提取到数据");
+            };
+            //读取块数据
+            Func<byte[]> readBlock = () =>
+            {
+                var len = readLen(0x02);
+                if (data[idx] == 0x00)
+                {
+                    idx++;
+                    len--;
+                }
+                var val = Extensions.sub(data, idx, len);
+                idx += len;
+                return val;
+            };
+            
+            //比较data从idx位置开始是否是byts内容
+            Func<byte[], bool> eq = (byts) =>
+            {
+                for (var i = 0; i < byts.Length; i++, idx++)
+                {
+                    if (idx >= data.Length)
+                    {
+                        return false;
+                    }
+                    if (byts[i] != data[idx])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            };
+            
+            /****使用公钥****/
+            //读取数据总长度
+            readLen(0x30);
+            if (!eq(Extensions._SeqOID))
+            {
+                throw new Exception("PEM未知格式");
+            }
+            //读取1长度
+            readLen(0x03);
+            idx++;//跳过0x00
+                  //读取2长度
+            readLen(0x30);
+            
+            var param = new RSAParameters();
+            //Modulus
+            param.Modulus = readBlock();
+            
+            //Exponent
+            param.Exponent = readBlock();
 
+            var rsa = new RSACryptoServiceProvider();
+            rsa.ImportParameters(param);
+            return rsa;
+        }
+           
         /// <summary>
         /// 将RSA中的密钥对转换成PEM格式
         /// </summary>
